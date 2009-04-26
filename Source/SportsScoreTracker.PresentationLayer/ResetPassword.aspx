9762f3bd78a7212072eafcdd7ef0d54894a9f49b
c0bra99 <c0bra99@505320d0-f726-11dd-88c7-29a3b14d5316>
2009-04-26 04:03:38 +0000

    Added a new page, ResetPassword

---
+++ Source/SportsScoreTracker.PresentationLayer/ResetPassword.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.Master" AutoEventWireup="true" CodeBehind="ResetPassword.aspx.cs" Inherits="SportsScoreTracker.PresentationLayer.ResetPassword" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <table>
+        <tr>
+            <td>
+                Please enter the email address you registered with:
+            </td>
+            <td>
+                <asp:TextBox ID="txtEmailAddress" runat="server" />
+            </td>
+        </tr>
+        <tr>
+            <td>
+            
+            </td>
+            <td>
+                <asp:Button ID="btnResetPassword" runat="server" Text="Reset Password" CssClass="Button" onclick="btnResetPassword_Click" />
+            </td>
+        </tr>
+    </table>
+    <asp:Label ID="lblErrorMessage" runat="server" EnableViewState="False" CssClass="Error" />
+</asp:Content>
---
+++ Source/SportsScoreTracker.PresentationLayer/ResetPassword.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using SportsScoreTracker.BusinessLogicLayer;
+
+namespace SportsScoreTracker.PresentationLayer
+{
+    public partial class ResetPassword : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        /// <summary>
+        /// Fires when a user clicks the button to reset their password
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        protected void btnResetPassword_Click(object sender, EventArgs e)
+        {
+            if (!RegisteredUser.ResetPassword(txtEmailAddress.Text))
+                lblErrorMessage.Text = "The email address you entered is not recognized.";
+            else
+                lblErrorMessage.Text = "An email has been sent with your new password.";
+        }
+    }
+}
