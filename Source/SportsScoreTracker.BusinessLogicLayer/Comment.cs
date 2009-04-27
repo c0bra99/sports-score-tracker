f88171b2c25905ea9b1a8bfdafdbbc74b56166c3
c0bra99 <c0bra99@505320d0-f726-11dd-88c7-29a3b14d5316>
2009-04-27 23:34:50 +0000

    Added comment category enum, changed GetCommentsByGameID, and added Delete method.

--- Source/SportsScoreTracker.BusinessLogicLayer/Comment.cs
+++ Source/SportsScoreTracker.BusinessLogicLayer/Comment.cs
@@ -8,6 +8,18 @@ namespace SportsScoreTracker.BusinessLogicLayer
 {
     public class Comment
     {
+        /// <summary>
+        /// The different categories a comment can belong to
+        /// </summary>
+        public enum Category
+        {
+            All = 0,
+            Injury = 1,
+            Score = 2,
+            Random = 3
+        }
+
+
         public int ID;
         public int UserID;
         public int GameID;
@@ -18,6 +30,10 @@ namespace SportsScoreTracker.BusinessLogicLayer
         public Comment() 
         { }
         
+
+        /// <summary>
+        /// Constructor taking all parameters
+        /// </summary>
         public Comment(int id, int userid, int gameid, short type, DateTime timestamp, string text)
         {
             ID = id;
@@ -28,21 +44,44 @@ namespace SportsScoreTracker.BusinessLogicLayer
             Text = text;
         }
 
-        public static List<Comment> GetCommentsByGameID(int gameID)
+
+        /// <summary>
+        /// Gets the comments for a single game
+        /// </summary>
+        public static List<DisplayComment> GetCommentsByGameID(int gameID)
         {
             SportsTrackerDBDataContext db = new SportsTrackerDBDataContext();
 
-            IEnumerable<Comment> comments = from c in db.GetCommentsByGameID(gameID)
-                                            select new Comment
+            IEnumerable<DisplayComment> comments = from o in db.GetCommentsByGameID(gameID)
+                                            select new DisplayComment
                                             {
-                                                ID = c.ID,
-                                                UserID = c.UserID,
-                                                GameID = c.GameID,
-                                                Type = (short)c.Type,
-                                                Timestamp = c.Timestamp,
-                                                Text = c.Text
+                                                CommentID = o.CommentID,
+                                                UserName = o.FirstName,
+                                                Type = o.Type.GetValueOrDefault(-1),
+                                                Timestamp = o.Timestamp,
+                                                Text = o.Text
                                             };
-            return (List<Comment>)comments;
+            return comments.ToList();
         }
+
+
+        /// <summary>
+        /// Deletes this comment
+        /// </summary>
+        public void Delete()
+        {
+            Delete(this.ID);
+        }
+
+
+        /// <summary>
+        /// Deletes a comment given the comment ID
+        /// </summary>
+        public static void Delete(int commentID)
+        {
+            SportsTrackerDBDataContext db = new SportsTrackerDBDataContext();
+            db.Comment_Delete(commentID);
+        }
+
     }
 }
---
+++ Source/SportsScoreTracker.BusinessLogicLayer/DisplayComment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SportsScoreTracker.BusinessLogicLayer
+{
+    /// <summary>
+    /// A class to hold the stuff that will be displayed with a comment
+    /// </summary>
+    public class DisplayComment
+    {
+        public string UserName;
+        public int CommentID;
+        public short Type;
+        public DateTime Timestamp;
+        public string Text;
+    }
+}
