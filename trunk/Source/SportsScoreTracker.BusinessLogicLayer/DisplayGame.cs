3a5ade4666cffa17d4b776bad9b7576389518173
c0bra99 <c0bra99@505320d0-f726-11dd-88c7-29a3b14d5316>
2009-04-28 02:55:34 +0000

    Added DisplayGame class

---
+++ trunk/Source/SportsScoreTracker.BusinessLogicLayer/DisplayGame.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SportsScoreTracker.BusinessLogicLayer
+{
+    /// <summary>
+    /// A class to hold the stuff that will be displayed with a game
+    /// </summary>
+    public class DisplayGame
+    {
+        public DateTime GameDate;
+        public string AwayTeam;
+        public string HomeTeam;
+        public int AwayScore;
+        public int HomeScore;
+        public int AwayTeamVotes;
+        public int HomeTeamVotes;
+        public int AwayWins;
+        public int AwayLosses;
+        public double AwayWinningPercentage;
+        public int HomeWins;
+        public int HomeLosses;
+        public double HomeWinningPercentage;
+    }
+}
