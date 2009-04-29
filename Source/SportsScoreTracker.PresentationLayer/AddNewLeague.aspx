7158a6401e1a4ed799805ffa48108ede726f7b48
c0bra99 <c0bra99@505320d0-f726-11dd-88c7-29a3b14d5316>
2009-04-29 03:10:32 +0000

    Implemented add new league logic

--- Source/SportsScoreTracker.PresentationLayer/AddNewLeague.aspx
+++ Source/SportsScoreTracker.PresentationLayer/AddNewLeague.aspx
@@ -1,3 +1,28 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.Master" AutoEventWireup="true" CodeBehind="AddNewLeague.aspx.cs" Inherits="SportsScoreTracker.PresentationLayer.AddNewLeague" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <h2>Enter New League Information:</h2>
+    <table>
+        <tr>
+            <td>
+                Sport: 
+            </td>
+            <td>
+                <asp:DropDownList ID="ddlSports" runat="server" />
+            </td>
+        </tr>
+        <tr>
+            <td>
+                Name: 
+            </td>
+            <td>
+                <asp:TextBox ID="txtName" runat="server" />
+            </td>
+        </tr>
+        <tr>
+            <td colspan="2" style="text-align:center;">
+                <asp:Button ID="btnSave" runat="server" Text="Save" CssClass="Button" 
+                    onclick="btnSave_Click" />
+            </td>
+        </tr>
+    </table>
 </asp:Content>
--- Source/SportsScoreTracker.PresentationLayer/AddNewLeague.aspx.cs
+++ Source/SportsScoreTracker.PresentationLayer/AddNewLeague.aspx.cs
@@ -4,14 +4,45 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using SportsScoreTracker.BusinessLogicLayer;
 
 namespace SportsScoreTracker.PresentationLayer
 {
     public partial class AddNewLeague : System.Web.UI.Page
     {
+        /// <summary>
+        /// Fires on page load
+        /// </summary>
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                List<Sport> sports = Sport.GetSports();
+                foreach (Sport sport in sports)
+                {
+                    ddlSports.Items.Add(new ListItem(sport.Name, sport.ID.ToString()));
+                }
+            }
+        }
 
+
+        /// <summary>
+        /// Fires when user clicks the save button
+        /// </summary>
+        protected void btnSave_Click(object sender, EventArgs e)
+        {
+            MasterPage master = (MasterPage)this.Master;
+
+            if (master.IsLoggedIn())
+            {
+                RegisteredUser user = master.GetLoggedInUser();
+                int sportID = int.Parse(ddlSports.SelectedValue);
+
+                League league = new League(sportID, user.ID, txtName.Text);
+                league.Save();
+            }
+
+            Response.Redirect(ResolveUrl("~/MyLeagues.aspx"));
         }
     }
 }
