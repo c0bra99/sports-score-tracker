b783554110f64709da8a563363609883a9efe20f
c0bra99 <c0bra99@505320d0-f726-11dd-88c7-29a3b14d5316>
2009-04-29 00:25:24 +0000

    Created class with save, delete, constructors, etc...

--- trunk/Source/SportsScoreTracker.BusinessLogicLayer/Prediction.cs
+++ trunk/Source/SportsScoreTracker.BusinessLogicLayer/Prediction.cs
@@ -2,10 +2,42 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using SportsScoreTracker.DataAccessLayer;
 
 namespace SportsScoreTracker.BusinessLogicLayer
 {
-    class Prediction
+    public class Prediction
     {
+        public int GameID;
+        public int UserID;
+        public int TeamID;
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public Prediction()
+        { }
+
+
+        /// <summary>
+        /// Constructor taking all parameters
+        /// </summary>
+        public Prediction(int gameid, int userid, int teamid)
+        {
+            GameID = gameid;
+            UserID = userid;
+            TeamID = teamid;
+        }
+
+
+        /// <summary>
+        /// Saves this new prediction to the database
+        /// </summary>
+        public void Save()
+        {
+            SportsTrackerDBDataContext db = new SportsTrackerDBDataContext();
+            db.Prediction_Insert(GameID, UserID, TeamID);
+        }
+
     }
 }
