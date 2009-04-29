66bc90775b3696f4d6c464d6f5ca33c390d8b2a9
c0bra99 <c0bra99@505320d0-f726-11dd-88c7-29a3b14d5316>
2009-04-29 00:25:46 +0000

    Created class with constructors, delete, save, etc...

--- trunk/Source/SportsScoreTracker.BusinessLogicLayer/Team.cs
+++ trunk/Source/SportsScoreTracker.BusinessLogicLayer/Team.cs
@@ -2,10 +2,82 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using SportsScoreTracker.DataAccessLayer;
 
 namespace SportsScoreTracker.BusinessLogicLayer
 {
-    class Team
+    /// <summary>
+    /// Class for an instance of a team
+    /// </summary>
+    public class Team
     {
+        public int ID;
+        public int LeagueID;
+        public string Name;
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public Team()
+        { }
+
+
+        /// <summary>
+        /// Constructor for a NEW instance of a team
+        /// </summary>
+        public Team(int leagueid, string name)
+        {
+            ID = -1;
+            LeagueID = leagueid;
+            Name = name;
+        }
+
+
+        /// <summary>
+        /// Constructor taking all parameters
+        /// </summary>
+        public Team(int id, int leagueid, string name)
+        {
+            ID = id;
+            LeagueID = leagueid;
+            Name = name;
+        }
+
+
+        /// <summary>
+        /// Deletes this team
+        /// </summary>
+        public void Delete()
+        {
+            Delete(this.ID);
+        }
+
+
+        /// <summary>
+        /// Deletes a team given the team ID
+        /// </summary>
+        public static void Delete(int teamID)
+        {
+            SportsTrackerDBDataContext db = new SportsTrackerDBDataContext();
+            db.Team_Delete(teamID);
+        }
+
+
+        /// <summary>
+        /// Saves the new / updated team info to the database
+        /// </summary>
+        public void Save()
+        {
+            SportsTrackerDBDataContext db = new SportsTrackerDBDataContext();
+
+            if (this.ID == -1) //new team, need to insert
+            {
+                db.Team_Insert(this.LeagueID, this.Name);
+            }
+            else //old team, need to update
+            {
+                db.Team_Update(this.ID, this.LeagueID, this.Name);
+            }
+        }
     }
 }
