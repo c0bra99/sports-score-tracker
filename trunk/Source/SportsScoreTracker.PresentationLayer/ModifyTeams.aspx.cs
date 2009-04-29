a6d5baee9eecdf6485b5e39c6738cc73155e0ced
phitaucox@gmail.com <phitaucox@gmail.com@505320d0-f726-11dd-88c7-29a3b14d5316>
2009-04-29 03:25:17 +0000

    updated edit team and modify teams

--- trunk/Source/SportsScoreTracker.PresentationLayer/ModifyTeams.aspx.cs
+++ trunk/Source/SportsScoreTracker.PresentationLayer/ModifyTeams.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using SportsScoreTracker.BusinessLogicLayer;
 
 namespace SportsScoreTracker.PresentationLayer
 {
@@ -11,7 +12,86 @@ namespace SportsScoreTracker.PresentationLayer
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            if (((MasterPage)this.Master).IsLoggedIn())
+            {
+                FillTeamsTable();
+            }
         }
+
+               /// <summary>
+        /// Fills the team table 
+        /// </summary>
+        private void FillTeamsTable()
+        {
+            int leagueID = int.Parse(Request.QueryString["LeagueID"]);
+
+            tblModifyTeams.Rows.Clear();
+
+            //setup the header
+            TableHeaderRow header = new TableHeaderRow();
+            tblModifyTeams.Rows.Add(header);
+
+            TableHeaderCell teamNameHeader = new TableHeaderCell();
+            teamNameHeader.Text = "Team Name";
+            header.Cells.Add(teamNameHeader);
+
+            TableHeaderCell editHeader = new TableHeaderCell();
+            editHeader.Text = "Edit";
+            header.Cells.Add(editHeader);
+
+            TableHeaderCell deleteHeader = new TableHeaderCell();
+            deleteHeader.Text = "Delete";
+            header.Cells.Add(deleteHeader);
+
+            List<Team> teams = Team.GetTeamsByLeagueID(leagueID);
+            foreach (Team team in teams)
+            {
+                if (team.LeagueID == leagueID)
+                {
+                    TableRow row = new TableRow();
+                    TableCell nameCell = new TableCell();
+                    row.Cells.Add(nameCell);
+                    nameCell.Text = team.Name;
+                 
+                    TableCell modifyCell = new TableCell();
+                    row.Cells.Add(modifyCell);
+                    
+                    ImageButton modifyTeamLink = new ImageButton();
+                    modifyTeamLink.ImageUrl = ResolveUrl("~/images/edit-icon.gif");
+                    modifyTeamLink.PostBackUrl = ResolveUrl("~/EditTeam.aspx") + "?TeamID=" + team.ID;
+
+                    modifyCell.Controls.Add(modifyTeamLink);
+                    
+                    TableCell deleteCell = new TableCell();
+                    row.Cells.Add(deleteCell);
+
+                    ImageButton deleteTeamLink = new ImageButton();
+                    deleteTeamLink.ImageUrl = ResolveUrl("~/images/deleteX.gif");
+                    deleteTeamLink.Click += new ImageClickEventHandler(deleteTeamLink_Click);
+                    deleteTeamLink.Attributes.Add("TeamID", team.ID.ToString());
+
+                    deleteCell.Controls.Add(deleteTeamLink);
+
+                    tblModifyTeams.Rows.Add(row);
+                }
+            }
+        }
+
+
+        /// <summary>
+        /// Fires when an admin clicks the red X to delete a league
+        /// </summary>
+        void deleteTeamLink_Click(object sender, ImageClickEventArgs e)
+        {
+            if (((MasterPage)this.Master).IsLoggedIn())
+            {
+                ImageButton deleteTeamLink = (ImageButton)sender;
+                int teamID = int.Parse(deleteTeamLink.Attributes["TeamID"]);
+
+                Team.Delete(teamID);
+                FillTeamsTable();
+            }
+        }
+
     }
 }
