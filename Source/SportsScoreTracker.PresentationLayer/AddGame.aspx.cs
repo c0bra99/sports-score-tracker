dcc2abe290ee77d441f7f27347c9ed144acd5f64
c0bra99 <c0bra99@505320d0-f726-11dd-88c7-29a3b14d5316>
2009-04-29 16:48:27 +0000

    Added controls to the design view, haven't coded for them though.

--- Source/SportsScoreTracker.PresentationLayer/AddGame.aspx.cs
+++ Source/SportsScoreTracker.PresentationLayer/AddGame.aspx.cs
@@ -10,7 +10,23 @@ namespace SportsScoreTracker.PresentationLayer
 {
     public partial class AddGame : System.Web.UI.Page
     {
+        /// <summary>
+        /// Fires on page load
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        /// <summary>
+        /// Fires when the user clicks the button to save the game
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        protected void btnSaveGame_Click(object sender, EventArgs e)
         {
 
         }
