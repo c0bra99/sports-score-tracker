51dbd6933ca1cfa893bc6a13df19196b9df02032
c0bra99 <c0bra99@505320d0-f726-11dd-88c7-29a3b14d5316>
2009-05-01 23:28:49 +0000

    Made it pass leagueID in the querystring to the AddGame page

--- Source/SportsScoreTracker.PresentationLayer/GamesByTeam.aspx.designer.cs
+++ Source/SportsScoreTracker.PresentationLayer/GamesByTeam.aspx.designer.cs
@@ -23,12 +23,12 @@ namespace SportsScoreTracker.PresentationLayer {
         protected global::System.Web.UI.WebControls.Table tblGames;
         
         /// <summary>
-        /// HyperLink1 control.
+        /// lnkAddNewGame control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.HyperLink HyperLink1;
+        protected global::System.Web.UI.WebControls.HyperLink lnkAddNewGame;
     }
 }
