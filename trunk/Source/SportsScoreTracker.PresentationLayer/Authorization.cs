b62fe19f33b27a3e943645a61aaf852ab81d41c3
c0bra99 <c0bra99@505320d0-f726-11dd-88c7-29a3b14d5316>
2009-05-01 23:28:17 +0000

    Added Authorization class to handle authorization

---
+++ trunk/Source/SportsScoreTracker.PresentationLayer/Authorization.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SportsScoreTracker.PresentationLayer
+{
+    public class Authorization
+    {
+        /// <summary>
+        /// The different types of authorization levels in our system.
+        /// </summary>
+        public enum AuthorizationLevel
+        {
+            RegisteredUser,
+            Administrator,
+            Guest
+        }
+
+
+        /// <summary>
+        /// Authorizes a user
+        /// </summary>
+        /// 
+        /// <param name="authLevel">This should be the minimum authorization level required for the page</param>
+        public static void Authorize(System.Web.UI.Page page, AuthorizationLevel authLevel)
+        {
+            if (page.Master != null && page.Master is MasterPage)
+            {
+                MasterPage master = (MasterPage)page.Master;
+                if (authLevel == AuthorizationLevel.Administrator)
+                {
+                    if (!master.IsAdminLoggedIn())
+                    {
+                        master.GoHome();
+                    }
+                }
+                else if (authLevel == AuthorizationLevel.RegisteredUser)
+                {
+                    if (!master.IsLoggedIn())
+                    {
+                        master.GoHome();
+                    }
+                }
+                //Anyone can view Guest level
+            }
+        }
+    }
+}
