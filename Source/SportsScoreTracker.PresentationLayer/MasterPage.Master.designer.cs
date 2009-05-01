f7ead1f52c85728b22470b877a7da387d2029e2c
c0bra99 <c0bra99@505320d0-f726-11dd-88c7-29a3b14d5316>
2009-05-01 23:36:22 +0000

    Made the banner link to home

--- Source/SportsScoreTracker.PresentationLayer/MasterPage.Master.designer.cs
+++ Source/SportsScoreTracker.PresentationLayer/MasterPage.Master.designer.cs
@@ -23,6 +23,15 @@ namespace SportsScoreTracker.PresentationLayer {
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
         
         /// <summary>
+        /// HyperLink3 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink HyperLink3;
+        
+        /// <summary>
         /// pnlLogin control.
         /// </summary>
         /// <remarks>
