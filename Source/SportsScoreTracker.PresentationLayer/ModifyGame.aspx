67468c1941fb104be7ae297cc5c432f3e5aee904
c0bra99 <c0bra99@505320d0-f726-11dd-88c7-29a3b14d5316>
2009-05-02 00:13:39 +0000

    Made it work.

--- Source/SportsScoreTracker.PresentationLayer/ModifyGame.aspx
+++ Source/SportsScoreTracker.PresentationLayer/ModifyGame.aspx
@@ -1,3 +1,62 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.Master" AutoEventWireup="true" CodeBehind="ModifyGame.aspx.cs" Inherits="SportsScoreTracker.PresentationLayer.ModifyGame" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <h2>Edit Game Information:</h2>
+    <table>
+        <tr>
+            <td>
+                Away Team:
+            </td>
+            <td>
+                <asp:Label ID="lblAway" runat="server" Text="Away Team Name" />
+            </td>
+        </tr>
+        <tr>
+            <td>
+                Home Team:
+            </td>
+            <td>
+                <asp:Label ID="lblHome" runat="server" Text="Home Team Name" />
+            </td>
+        </tr>
+        <tr>
+            <td>
+                Game Date:
+            </td>
+            <td>
+                <asp:Calendar ID="calDate" runat="server" />
+            </td>
+        </tr>
+        <tr>
+            <td>
+                Game Time:
+            </td>
+            <td>
+                <asp:DropDownList ID="ddlHour" runat="server" />&nbsp;:&nbsp;
+                <asp:DropDownList ID="ddlMinute" runat="server" />&nbsp;
+            </td>
+        </tr>
+        <tr>
+            <td>
+                Away Score (if known):
+            </td>
+            <td>
+                <asp:TextBox ID="txtAwayScore" runat="server" />
+                <asp:RegularExpressionValidator ID="RegularExpressionValidator2" runat="server" ControlToValidate="txtAwayScore" ErrorMessage="Score must be numeric" ValidationExpression="[0-9]*" />
+            </td>
+        </tr>
+        <tr>
+            <td>
+                Home Score (if known):
+            </td>
+            <td>
+                <asp:TextBox ID="txtHomeScore" runat="server" />
+                       <asp:RegularExpressionValidator ID="RegularExpressionValidator1" runat="server" ControlToValidate="txtHomeScore" ErrorMessage="Score must be numeric" ValidationExpression="[0-9]*" />
+            </td>
+        </tr>
+        <tr>
+            <td colspan="2" style="text-align:center;">
+                <asp:Button ID="btnSaveGame" runat="server" Text="Save Game" onclick="btnSaveGame_Click" CssClass="Button" />
+            </td>
+        </tr>
+    </table>
 </asp:Content>
--- Source/SportsScoreTracker.PresentationLayer/ModifyGame.aspx.cs
+++ Source/SportsScoreTracker.PresentationLayer/ModifyGame.aspx.cs
@@ -10,9 +10,63 @@ namespace SportsScoreTracker.PresentationLayer
 {
     public partial class ModifyGame : System.Web.UI.Page
     {
+        /// <summary>
+        /// Fires on page load
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                for (int i = 0; i < 23; i++)
+                {
+                    ddlHour.Items.Add(new ListItem(i.ToString(), i.ToString()));
+                }
+                for (int i = 0; i < 59; i++)
+                {
+                    ddlMinute.Items.Add(new ListItem(i.ToString(), i.ToString()));
+                }
 
+                int gameID = int.Parse(Request.QueryString["GameID"]);
+                Game game = Game.GetGame(gameID);
+                Team awayTeam = Team.GetTeam(game.AwayTeamID);
+                Team homeTeam = Team.GetTeam(game.HomeTeamID);
+                lblAway.Text = awayTeam.Name;
+                lblHome.Text = homeTeam.Name;
+                calDate.SelectedDate = game.Date;
+                ddlHour.SelectedValue = game.Date.Hour.ToString();
+                ddlMinute.SelectedValue = game.Date.Minute.ToString();
+                txtAwayScore.Text = game.AwayScore.ToString();
+                txtHomeScore.Text = game.HomeScore.ToString();
+            }
+        }
+
+
+        /// <summary>
+        /// Fires when the user clicks the save button to update the game
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        protected void btnSaveGame_Click(object sender, EventArgs e)
+        {
+            int hour = int.Parse(ddlHour.SelectedValue);
+            int minute = int.Parse(ddlMinute.SelectedValue);
+            DateTime gameDate = new DateTime(calDate.SelectedDate.Year, calDate.SelectedDate.Month, calDate.SelectedDate.Day, hour, minute, 0);
+            int awayScore = 0;
+            int homeScore = 0;
+            int.TryParse(txtAwayScore.Text, out awayScore);
+            int.TryParse(txtHomeScore.Text, out homeScore);
+
+            int gameID = int.Parse(Request.QueryString["GameID"]);
+            Game game = Game.GetGame(gameID);
+            game.HomeScore = homeScore;
+            game.AwayScore = awayScore;
+            game.Date = gameDate;
+            game.Save();
+
+            int leagueID = int.Parse(Request.QueryString["LeagueID"]);
+            Response.Redirect(ResolveUrl("~/GamesByTeam.aspx") + "?TeamID=" + game.HomeTeamID + "&LeagueID=" + leagueID);
         }
     }
 }
