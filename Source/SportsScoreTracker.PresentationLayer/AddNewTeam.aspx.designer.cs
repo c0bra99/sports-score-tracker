0e3616b29e8c747f67436cd6381a801a06150f2d
c0bra99 <c0bra99@505320d0-f726-11dd-88c7-29a3b14d5316>
2009-05-04 00:59:06 +0000

    Added a required field validator

--- Source/SportsScoreTracker.PresentationLayer/AddNewTeam.aspx.designer.cs
+++ Source/SportsScoreTracker.PresentationLayer/AddNewTeam.aspx.designer.cs
@@ -32,6 +32,15 @@ namespace SportsScoreTracker.PresentationLayer {
         protected global::System.Web.UI.WebControls.DropDownList ddlLeagues;
         
         /// <summary>
+        /// RequiredFieldValidator1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator RequiredFieldValidator1;
+        
+        /// <summary>
         /// txtName control.
         /// </summary>
         /// <remarks>
